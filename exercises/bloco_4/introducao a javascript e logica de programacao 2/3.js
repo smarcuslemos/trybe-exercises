232a57036cd92fd9634088732e3ee4f0e72982c3
Marcus Santos <smarcuslemos@gmail.com>
2021-04-23 18:34:37 -0300

    Questoes 1 ao 3 resolvidas.

---
+++ exercises/bloco_4/introducao a javascript e logica de programacao 2/3.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+
+for (let contador = 0; contador < numbers.length; contador += 1){
+    soma += numbers[contador];
+}
+
+let mediaAritmetica = soma / numbers.length;
+
+console.log(mediaAritmetica);
+
