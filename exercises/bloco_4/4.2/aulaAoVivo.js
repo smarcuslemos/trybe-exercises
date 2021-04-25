fb229ce5b8dc8da1d278e268f8a4d5edb87ea09b
Marcus Santos <smarcuslemos@gmail.com>
2021-04-25 20:56:32 -0300

    Gera numeros aleatorios de 1 a 60.

--- exercises/bloco_4/4.2/aulaAoVivo.js
+++ exercises/bloco_4/4.2/aulaAoVivo.js
@@ -4,3 +4,15 @@ Sortear o mesmo intervalo.
 Ganha quem acertar 6 números.
 */
 
+let megaSenaNumbers = [];
+
+let firstNumber = Math.ceil(Math.random() * 60);
+let secondNumber = Math.ceil(Math.random() * 60);
+let thirdNumber = Math.ceil(Math.random() * 60);
+let fourthNumber = Math.ceil(Math.random() * 60);
+let fifthNumber = Math.ceil(Math.random() * 60);
+let sixthNumber = Math.ceil(Math.random() * 60);
+
+megaSenaNumbers = [firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber];
+
+console.log(megaSenaNumbers);
