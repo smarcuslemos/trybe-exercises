42b4559907931566a812d50b2a7b04c92c24c919
Marcus Santos <smarcuslemos@gmail.com>
2021-04-25 21:40:30 -0300

    Add checador de MegaSena.

--- exercises/bloco_4/4.2/aulaAoVivo.js
+++ exercises/bloco_4/4.2/aulaAoVivo.js
@@ -15,4 +15,18 @@ let sixthNumber = Math.ceil(Math.random() * 60);
 
 megaSenaNumbers = [firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber];
 
-console.log(megaSenaNumbers);
+let turma12Play = [35, 8, 22, 14, 2, 60];
+
+let numberOfHits = 0;
+
+for (let indexMega = 0; indexMega < megaSenaNumbers.length; indexMega += 1) {
+    for (let indexPlay = 0; indexPlay < turma12Play.length; indexPlay += 1) {
+        if(megaSenaNumbers[indexMega] === turma12Play[indexPlay]){
+            numberOfHits += 1;
+        }
+    }
+}
+
+console.log(megaSenaNumbers)
+console.log(turma12Play)
+console.log(numberOfHits)
