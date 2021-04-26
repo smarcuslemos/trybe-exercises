9caffd17d5c9df04a84bceb0793755b2d0d1951c
Marcus Santos <smarcuslemos@gmail.com>
2021-04-26 17:34:50 -0300

    Quase finalizado o xadrez da Rainha. Previso revisar.

--- exercises/bloco_4/4.3/aula-ao-vivo.js
+++ exercises/bloco_4/4.3/aula-ao-vivo.js
@@ -5,8 +5,8 @@ let posicaoRainhaLinha = 4;
 let posicaoRainhaColuna = 4;
 
 //Posição peça alvo
-let posicaoPecaLinha = ;
-let posicaoPecaColuna = ;
+let posicaoPecaLinha = 3;
+let posicaoPecaColuna = 2;
 
 let ataqueBemSucedido = false;
 
@@ -20,6 +20,20 @@ for (let supDir = 1; supDir < 8; supDir += 1) {
     let colunaRainha = posicaoRainhaColuna + supDir;
 
     if (linhaRainha > 8 || colunaRainha > 8) {
+        break;
+    };
+
+    if ((posicaoPecaLinha === linhaRainha) && (posicaoPecaColuna == colunaRainha)) {
+        ataqueBemSucedido = true;
+    };
+};
+
+//Diagonal superior esquerda
+for (let infEsq = 1; infEsq < 8; infEsq += 1) {
+    let linhaRainha = posicaoRainhaLinha - supEsq;
+    let colunaRainha = posicaoRainhaColuna - supEsq;
+
+    if (linhaRainha < 1 || colunaRainha < 1) {
         break;
     };
 
