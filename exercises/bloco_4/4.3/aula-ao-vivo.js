db974e96fd82053da82b23e3c535e33565208eef
Marcus Santos <smarcuslemos@gmail.com>
2021-04-26 17:06:44 -0300

    Adiciona movimento horizontal, vertical e diagonal.

--- exercises/bloco_4/4.3/aula-ao-vivo.js
+++ exercises/bloco_4/4.3/aula-ao-vivo.js
@@ -1,4 +1,31 @@
-/*
-Movimentos da Rainha
-*/
+//Movimentos da Rainha
 
+//Posição da Rainha
+let posicaoRainhaLinha = 4;
+let posicaoRainhaColuna = 4;
+
+//Posição peça alvo
+let posicaoPecaLinha = ;
+let posicaoPecaColuna = ;
+
+let ataqueBemSucedido = false;
+
+if (posicaoRainhaLinha === posicaoPecaLinha || posicaoRainhaColuna === posicaoPecaColuna){
+    ataqueBemSucedido = true;
+};
+
+//Diagonal superior direita
+for (let supDir = 1; supDir < 8; supDir += 1) {
+    let linhaRainha = posicaoRainhaLinha + supDir;
+    let colunaRainha = posicaoRainhaColuna + supDir;
+
+    if (linhaRainha > 8 || colunaRainha > 8) {
+        break;
+    };
+
+    if ((posicaoPecaLinha === linhaRainha) && (posicaoPecaColuna == colunaRainha)) {
+        ataqueBemSucedido = true;
+    };
+};
+
+console.log(ataqueBemSucedido)
