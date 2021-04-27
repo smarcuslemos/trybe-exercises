5e4b74f0ccbf40c4f69f23655b40cb338db4855b
Marcus Santos <smarcuslemos@gmail.com>
2021-04-27 18:17:27 -0300

    Exercicio 1 feito.

---
+++ exercises/bloco_4/4.4/exercicios/introducao-a-javascript-e-logica-de-programacao_4/Parte I - Objetos e For/Parte I - Objetos e For/In/1.js
@@ -0,0 +1,8 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+
+  console.log('Boas-vinda ' + info.personagem + '.');
